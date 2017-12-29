c9f063906ac93e956cb13eae3e48750eaa5a3ccd
Matteo Marinelli <34916066+MatteoMarinelli@users.noreply.github.com>
2017-12-29 22:57:22 +0100

    Update MainActivity.java

--- app/src/main/java/it/marinelli/lamaantiruggine/MainActivity.java
+++ app/src/main/java/it/marinelli/lamaantiruggine/MainActivity.java
@@ -2,7 +2,6 @@ package it.marinelli.lamaantiruggine;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -23,15 +22,6 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
